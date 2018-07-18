9392b250976d559836f9d0e18157071826df81e7
Ilya <semjef@yandex.ru>
2018-07-18 20:01:24 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'samsungac',
+    version = '0.1',
+    url = 'https://github.com/semjef/samsungac/new/master',
+    author = 'Semjef',
+    author_email = 'semjef02@gmail.com’,
+    description = 'Samsung AC',
+    packages = find_packages(),
+)
